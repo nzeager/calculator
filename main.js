57c2844f9a3aa2890e8d91bd3cc5b4e3b8de4e39
Nathan Zeager <nathanzeager@hotmail.com>
2022-07-27 15:45:38 -0400

    Cleaned up JS and added output for numeric buttons

--- main.js
+++ main.js
@@ -1,56 +1,49 @@
-const Button0 = document.getElementById("num0");
-const Button1 = document.getElementById("num1");
-const Button2 = document.getElementById("num2");
-const Button3 = document.getElementById("num3");
-const Button4 = document.getElementById("num4");
-const Button5 = document.getElementById("num5");
-const Button6 = document.getElementById("num6");
-const Button7 = document.getElementById("num7");
-const Button8 = document.getElementById("num8");
-const Button9 = document.getElementById("num9");
-const ButtonC = document.getElementById("c");
-const ButtonDec = document.getElementById("dec");
-const ButtonEq = document.getElementById("eq");
-const ButtonAdd = document.getElementById("add");
-const ButtonSubt = document.getElementById("subt");
-const ButtonMult = document.getElementById("mult");
-const ButtonDiv = document.getElementById("div");
+const buttons = document.querySelectorAll(".button");
+const buttonOps = document.querySelectorAll(".buttonOp");
+const nums = document.querySelectorAll(".num");
+const Output = document.getElementById("screen");
 
-Button0.addEventListener("mousedown",colorChange);
-Button0.addEventListener("mouseup",colorRevert);
-Button1.addEventListener("mousedown",colorChange);
-Button1.addEventListener("mouseup",colorRevert);
-Button2.addEventListener("mousedown",colorChange);
-Button2.addEventListener("mouseup",colorRevert);
-Button3.addEventListener("mousedown",colorChange);
-Button3.addEventListener("mouseup",colorRevert);
-Button4.addEventListener("mousedown",colorChange);
-Button4.addEventListener("mouseup",colorRevert);
-Button5.addEventListener("mousedown",colorChange);
-Button5.addEventListener("mouseup",colorRevert);
-Button6.addEventListener("mousedown",colorChange);
-Button6.addEventListener("mouseup",colorRevert);
-Button7.addEventListener("mousedown",colorChange);
-Button7.addEventListener("mouseup",colorRevert);
-Button8.addEventListener("mousedown",colorChange);
-Button8.addEventListener("mouseup",colorRevert);
-Button9.addEventListener("mousedown",colorChange);
-Button9.addEventListener("mouseup",colorRevert);
-ButtonC.addEventListener("mousedown",colorChange);
-ButtonC.addEventListener("mouseup",colorRevert);
-ButtonDec.addEventListener("mousedown",colorChange);
-ButtonDec.addEventListener("mouseup",colorRevert);
-ButtonEq.addEventListener("mousedown",colorChange);
-ButtonEq.addEventListener("mouseup",colorRevert);
-ButtonAdd.addEventListener("mousedown",colorChange);
-ButtonAdd.addEventListener("mouseup",colorRevertOp);
-ButtonSubt.addEventListener("mousedown",colorChange);
-ButtonSubt.addEventListener("mouseup",colorRevertOp);
-ButtonMult.addEventListener("mousedown",colorChange);
-ButtonMult.addEventListener("mouseup",colorRevertOp);
-ButtonDiv.addEventListener("mousedown",colorChange);
-ButtonDiv.addEventListener("mouseup",colorRevertOp);
+// const Button0 = document.getElementById("num0");
+// const Button1 = document.getElementById("num1");
+// const Button2 = document.getElementById("num2");
+// const Button3 = document.getElementById("num3");
+// const Button4 = document.getElementById("num4");
+// const Button5 = document.getElementById("num5");
+// const Button6 = document.getElementById("num6");
+// const Button7 = document.getElementById("num7");
+// const Button8 = document.getElementById("num8");
+// const Button9 = document.getElementById("num9");
+// const ButtonC = document.getElementById("c");
+// const ButtonDec = document.getElementById("dec");
+// const ButtonEq = document.getElementById("eq");
+// const ButtonAdd = document.getElementById("add");
+// const ButtonSubt = document.getElementById("subt");
+// const ButtonMult = document.getElementById("mult");
+// const ButtonDiv = document.getElementById("div");
 
+// Changes color when you press/release buttons
+for(let button of buttons) {
+    button.addEventListener("mousedown",colorChange);
+}
+
+for(let buttonOp of buttonOps) {
+    buttonOp.addEventListener("mousedown",colorChange);
+}
+
+for(let button of buttons) {
+    button.addEventListener("mouseup",colorRevert);
+}
+
+for(let buttonOp of buttonOps) {
+    buttonOp.addEventListener("mouseup",colorRevertOp);
+}
+
+// Updates output when numbered buttons are pressed
+for(let num of nums) {
+    num.addEventListener("mousedown",calcInputNum);
+}
+
+// FUNCTIONS
 // makes button darker when you click down mouse
 function colorChange(e) {
     e.target.style.backgroundColor = "#198E16";
@@ -64,4 +57,13 @@ function colorRevert(e) {
 // reverts operation button color when you release mouse click
 function colorRevertOp(e) {
     e.target.style.backgroundColor = "#82c276";
+}
+
+// functions to input numeric values
+function calcInputNum(e) {
+    if (Output.innerText === "0") {
+        Output.innerText = e.target.innerText;
+    } else {
+        Output.innerText = Output.innerText + e.target.innerText;
+    }
 }
