67def744665471c8c66f749e9f29c64e00e39d9f
Nathan Zeager <nathanzeager@hotmail.com>
2022-07-27 09:00:12 -0400

    Finished button push animation

--- main.js
+++ main.js
@@ -16,15 +16,52 @@ const ButtonSubt = document.getElementById("subt");
 const ButtonMult = document.getElementById("mult");
 const ButtonDiv = document.getElementById("div");
 
+Button0.addEventListener("mousedown",colorChange);
+Button0.addEventListener("mouseup",colorRevert);
+Button1.addEventListener("mousedown",colorChange);
+Button1.addEventListener("mouseup",colorRevert);
+Button2.addEventListener("mousedown",colorChange);
+Button2.addEventListener("mouseup",colorRevert);
+Button3.addEventListener("mousedown",colorChange);
+Button3.addEventListener("mouseup",colorRevert);
+Button4.addEventListener("mousedown",colorChange);
+Button4.addEventListener("mouseup",colorRevert);
+Button5.addEventListener("mousedown",colorChange);
+Button5.addEventListener("mouseup",colorRevert);
+Button6.addEventListener("mousedown",colorChange);
+Button6.addEventListener("mouseup",colorRevert);
+Button7.addEventListener("mousedown",colorChange);
+Button7.addEventListener("mouseup",colorRevert);
+Button8.addEventListener("mousedown",colorChange);
+Button8.addEventListener("mouseup",colorRevert);
+Button9.addEventListener("mousedown",colorChange);
+Button9.addEventListener("mouseup",colorRevert);
 ButtonC.addEventListener("mousedown",colorChange);
 ButtonC.addEventListener("mouseup",colorRevert);
+ButtonDec.addEventListener("mousedown",colorChange);
+ButtonDec.addEventListener("mouseup",colorRevert);
+ButtonEq.addEventListener("mousedown",colorChange);
+ButtonEq.addEventListener("mouseup",colorRevert);
+ButtonAdd.addEventListener("mousedown",colorChange);
+ButtonAdd.addEventListener("mouseup",colorRevertOp);
+ButtonSubt.addEventListener("mousedown",colorChange);
+ButtonSubt.addEventListener("mouseup",colorRevertOp);
+ButtonMult.addEventListener("mousedown",colorChange);
+ButtonMult.addEventListener("mouseup",colorRevertOp);
+ButtonDiv.addEventListener("mousedown",colorChange);
+ButtonDiv.addEventListener("mouseup",colorRevertOp);
 
 // makes button darker when you click down mouse
 function colorChange(e) {
     e.target.style.backgroundColor = "#198E16";
 }
 
-// reverts button color when you release mouse click
+// reverts general button color when you release mouse click
 function colorRevert(e) {
     e.target.style.backgroundColor = "#61b34f";
+}
+
+// reverts operation button color when you release mouse click
+function colorRevertOp(e) {
+    e.target.style.backgroundColor = "#82c276";
 }
