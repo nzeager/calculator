1f22c8dcffe208fa06626e774b4b3b9eb77b8d03
Nathan Zeager <nathanzeager@hotmail.com>
2022-07-27 16:35:46 -0400

    Button inputs function except for equal button

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const buttons = document.querySelectorAll(".button");
 const buttonOps = document.querySelectorAll(".buttonOp");
 const nums = document.querySelectorAll(".num");
 const Output = document.getElementById("screen");
+const buttonDec = document.getElementById("dec");
+const buttonEq = document.getElementById("eq");
+const buttonC = document.getElementById("c");
 
 // const Button0 = document.getElementById("num0");
 // const Button1 = document.getElementById("num1");
@@ -13,14 +16,12 @@ const Output = document.getElementById("screen");
 // const Button7 = document.getElementById("num7");
 // const Button8 = document.getElementById("num8");
 // const Button9 = document.getElementById("num9");
-// const ButtonC = document.getElementById("c");
-// const ButtonDec = document.getElementById("dec");
-// const ButtonEq = document.getElementById("eq");
 // const ButtonAdd = document.getElementById("add");
 // const ButtonSubt = document.getElementById("subt");
 // const ButtonMult = document.getElementById("mult");
 // const ButtonDiv = document.getElementById("div");
 
+// --------EVENT LISTENERS--------
 // Changes color when you press/release buttons
 for(let button of buttons) {
     button.addEventListener("mousedown",colorChange);
@@ -40,10 +41,24 @@ for(let buttonOp of buttonOps) {
 
 // Updates output when numbered buttons are pressed
 for(let num of nums) {
-    num.addEventListener("mousedown",calcInputNum);
+    num.addEventListener("mousedown",inputNum);
 }
 
-// FUNCTIONS
+// Clears output when decimal button is pressed
+buttonDec.addEventListener("mousedown",inputDec);
+
+// Updates output when operation buttons are pressed
+for(let buttonOp of buttonOps) {
+    buttonOp.addEventListener("mousedown",inputOp);
+}
+
+// Calculates output when equal button is pressed
+buttonC.addEventListener("mousedown",calcOutput);
+
+// Clears output when C button is pressed
+buttonC.addEventListener("mousedown",clearOutput);
+
+// --------FUNCTIONS--------
 // makes button darker when you click down mouse
 function colorChange(e) {
     e.target.style.backgroundColor = "#198E16";
@@ -59,11 +74,35 @@ function colorRevertOp(e) {
     e.target.style.backgroundColor = "#82c276";
 }
 
-// functions to input numeric values
-function calcInputNum(e) {
+// function to input numeric values
+function inputNum(e) {
     if (Output.innerText === "0") {
         Output.innerText = e.target.innerText;
     } else {
         Output.innerText = Output.innerText + e.target.innerText;
     }
+}
+
+// function to input decimal
+function inputDec() {
+    Output.innerText = Output.innerText + ".";
+}
+
+// function to input operations
+function inputOp(e) {
+    if (e.target.innerText !== "X") {
+        Output.innerText = Output.innerText + e.target.innerText;
+    } else {
+        Output.innerText = Output.innerText + "*";
+    }
+}
+
+// function to calculate input (for equal button)
+function calcOutput() {
+    
+}
+
+// function to clear output
+function clearOutput() {
+    Output.innerText = "0"
 }
