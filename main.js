3d9e293e58d635378aeb6133f934616bf300d835
Nathan Zeager <nathanzeager@hotmail.com>
2022-07-28 14:47:25 -0400

    Added functionality to inputOp() function

--- main.js
+++ main.js
@@ -6,21 +6,6 @@ const buttonDec = document.getElementById("dec");
 const buttonEq = document.getElementById("eq");
 const buttonC = document.getElementById("c");
 
-// const Button0 = document.getElementById("num0");
-// const Button1 = document.getElementById("num1");
-// const Button2 = document.getElementById("num2");
-// const Button3 = document.getElementById("num3");
-// const Button4 = document.getElementById("num4");
-// const Button5 = document.getElementById("num5");
-// const Button6 = document.getElementById("num6");
-// const Button7 = document.getElementById("num7");
-// const Button8 = document.getElementById("num8");
-// const Button9 = document.getElementById("num9");
-// const ButtonAdd = document.getElementById("add");
-// const ButtonSubt = document.getElementById("subt");
-// const ButtonMult = document.getElementById("mult");
-// const ButtonDiv = document.getElementById("div");
-
 // --------EVENT LISTENERS--------
 // Changes color when you press/release buttons
 for(let button of buttons) {
@@ -86,7 +71,7 @@ function inputNum(e) {
 // function to input decimal 
 function inputDec() {
     if (Output.innerText.charAt(Output.innerText.length - 1) === ".") {
-        //doesn't allow you to put two decimals next to each other
+        // doesn't allow you to put two decimals next to each other
         Output.innerText = Output.innerText;
     } else if (isNaN(Number(Output.innerText.charAt(Output.innerText.length - 1)))) {
         // adds leading 0 if decimal comes at beginning of number
@@ -98,9 +83,14 @@ function inputDec() {
 
 // function to input operations
 function inputOp(e) {
-    if (e.target.innerText !== "X") {
+    if (isNaN(Number(Output.innerText.charAt(Output.innerText.length - 1)))) {
+        // doesn't allow you to put operation next to other operation or decimal
+        Output.innerText = Output.innerText;
+    } else if (e.target.innerText !== "X") {
+        // inputs operation, except for multiplication
         Output.innerText = Output.innerText + e.target.innerText;
     } else {
+        // inputs correct multiplication symbol
         Output.innerText = Output.innerText + "*";
     }
 }
